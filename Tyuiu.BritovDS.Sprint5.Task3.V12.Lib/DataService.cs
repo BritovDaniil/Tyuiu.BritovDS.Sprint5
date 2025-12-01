97d432dd623fb964036e0c9f8167f81eacfd9b2a
user <user@WIN-FJ5S0KDD7IV>
2025-12-01 20:09:54 +0300

    Task3

---
+++ Tyuiu.BritovDS.Sprint5.Task3.V12.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿namespace Tyuiu.BritovDS.Sprint5.Task3.V12.Lib;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+public class DataService : ISprint5Task3V12
+{
+    public string SaveToFileTextData(int x)
+    {
+        string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+        double y = Math.Round(Math.Pow(x, 3) / (2 * Math.Pow(x + 5, 2)), 3);
+
+        using (var bw = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+        {
+            bw.Write(y);
+        }
+
+        return path;
+    }
+}
---
+++ Tyuiu.BritovDS.Sprint5.Task3.V12/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.BritovDS.Sprint5.Task3.V12.Lib;
+
+Console.Title = "Спринт #5 | Выполнил: Бритов Д. С. | ИСПб-25-1";
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* Спринт #5                                                            *");
+Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                  *");
+Console.WriteLine("* Задание #3                                                           *");
+Console.WriteLine("* Вариант #12                                                          *");
+Console.WriteLine("* Выполнил: Бритов Д. С. | ИСПб-25-1                                   *");
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                             *");
+Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить *");
+Console.WriteLine("* в бинарный файл OutPutFileTask3.bin и вывести на консоль.            *");
+Console.WriteLine("* Округлить до трёх знаков после запятой.                              *");
+Console.WriteLine("************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+Console.WriteLine("************************************************************************");
+
+double result;
+using (var br = new BinaryReader(File.Open(new DataService().SaveToFileTextData(3), FileMode.Open)))
+{
+    result = br.ReadDouble();
+}
+Console.WriteLine(result);
+Console.ReadLine();
