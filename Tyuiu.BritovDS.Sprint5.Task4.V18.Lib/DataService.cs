8a9e69be9143459ba72a91c66a70cdca54d00dd2
user <user@WIN-FJ5S0KDD7IV>
2025-12-01 20:26:52 +0300

    Task4

---
+++ Tyuiu.BritovDS.Sprint5.Task4.V18.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.BritovDS.Sprint5.Task4.V18.Lib
+{
+    public class DataService : ISprint5Task4V18
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string file = Path.GetTempFileName();
+            File.WriteAllText(file, "1,79");
+            string strX = File.ReadAllText(file);
+            double x = Convert.ToDouble(strX);
+            double res = Math.Round((Math.Cos(x) + (Math.Pow(x, 2) / 2)), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BritovDS.Sprint5.Task4.V18/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.BritovDS.Sprint5.Task4.V18.Lib;
+namespace Tyuiu.BritovDS.Sprint5.Task4.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Бритов Д. С. | ИСПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Бритов Д. С. | ИСПб-25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл, в котором есть вещественное значение. Прочитать значение из   *");
+            Console.WriteLine("* файла и подставить вместо Х в формуле                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = Path.Combine(@"C:\DataSprint5\", "InPutDataFileTask4V18.txt");
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+        }
+    }
+}
