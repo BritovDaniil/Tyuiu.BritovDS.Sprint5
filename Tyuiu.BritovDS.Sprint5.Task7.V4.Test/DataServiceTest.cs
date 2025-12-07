96ca813bd3586183bbee9e338cb8f8c0dc030bcf
user <user@WIN-FJ5S0KDD7IV>
2025-12-07 13:12:17 +0300

    Task7

---
+++ Tyuiu.BritovDS.Sprint5.Task7.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BritovDS.Sprint5.Task7.V4.Lib;
+
+namespace Tyuiu.BritovDS.Sprint5.Task7.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Cruise\AppData\Local\Temp\OutPutDataFileTask7V4.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
